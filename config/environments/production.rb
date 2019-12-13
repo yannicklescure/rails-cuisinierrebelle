bb2e7df6fcdb29ed3c2b75edb96e76128d39f347
Yannick Lescure <yannick.lescure@gmail.com>
2019-12-13 15:49:42 -0500

    email postmark edits

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -34,7 +34,7 @@ Rails.application.configure do
   # config.action_mailer.postmark_settings = { :api_token => Rails.application.secrets.postmark_api_token }
   config.action_mailer.delivery_method     = :postmark
   config.action_mailer.postmark_settings   = { api_token: ENV['POSTMARK_API_TOKEN'] }
-  config.action_mailer.default_url_options = { host: "yannicklescure.herokuapp.com" }
+  config.action_mailer.default_url_options = { host: "yannicklescure.com" }
 
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
