1a5ccdd2f549a0321fabcb27b5aa77f48f78ed3e
yannick lescure <yannick.lescure@gmail.com>
2019-12-19 12:26:01 -0500

    create recipe model

---
+++ db/migrate/20191219170935_create_recipes.rb
@@ -0,0 +1,14 @@
+class CreateRecipes < ActiveRecord::Migration[5.2]
+  def change
+    create_table :recipes do |t|
+      t.string :title
+      t.string :subtitle
+      t.string :video
+      t.text :ingredients
+      t.text :direction
+      t.text :description
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_12_171017) do
+ActiveRecord::Schema.define(version: 2019_12_19_170935) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "recipes", force: :cascade do |t|
+    t.string "title"
+    t.string "subtitle"
+    t.string "video"
+    t.text "ingredients"
+    t.text "direction"
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
