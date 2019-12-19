9ac371d45c961ef0fcc75927f02612850a2c0be8
yannick lescure <yannick.lescure@gmail.com>
2019-12-19 11:41:42 -0500

    add cta slide + fix cta btn UI

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,2 +1,8 @@
 import "bootstrap";
 import { scrollToAnchor } from "../components/scroll-to-anchor";
+
+scrollToAnchor("#recipes-cards");
+
+const bannerCtaBox = document.querySelector('#banner-cta-box');
+const bannerCtaBoxBtn = document.querySelector('#banner-cta-box-btn');
+bannerCtaBoxBtn.style.width = `${bannerCtaBox.offsetWidth}px`;
