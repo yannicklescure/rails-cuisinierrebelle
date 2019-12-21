be68c050d722e25f709ee8bd37b0c764efc8a445
yannick lescure <yannick.lescure@gmail.com>
2019-12-21 13:03:10 -0500

    add confirmable to devise

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,5 +1,5 @@
 Rails.application.configure do
-  config.action_mailer.default_url_options = { host: "https://yannicklescure.com" }
+  config.action_mailer.default_url_options = { host: "http://localhost:3000" }
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded on
@@ -38,12 +38,6 @@ Rails.application.configure do
 
   # Add this line next to existing config.action_mailer settings
   config.action_mailer.delivery_method = :letter_opener
-
-  # config.action_mailer.delivery_method = :postmark
-  # config.action_mailer.postmark_settings = { :api_token => Rails.application.secrets.postmark_api_token }
-  config.action_mailer.delivery_method     = :postmark
-  config.action_mailer.postmark_settings   = { api_token: ENV['POSTMARK_API_TOKEN'] }
-  # config.action_mailer.default_url_options = { host: "yannicklescure.com" }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
