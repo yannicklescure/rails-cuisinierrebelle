449b0fbd2265536068aaec4af0c19c51167053bf
yannick lescure <yannick.lescure@gmail.com>
2019-12-21 14:21:12 -0500

    add name to user on registration

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -2,10 +2,12 @@ class RegistrationsController < Devise::RegistrationsController
   private
 
   def sign_up_params
-    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
+    params["user"]["name"] = "#{params["user"]["first_name"]} #{params["user"]["last_name"]}"
+    params.require(:user).permit(:first_name, :last_name, :name, :email, :password, :password_confirmation)
   end
 
   def account_update_params
-    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation, :current_password)
+    params["user"]["name"] = "#{params["user"]["first_name"]} #{params["user"]["last_name"]}"
+    params.require(:user).permit(:first_name, :last_name, :name, :email, :password, :password_confirmation, :current_password)
   end
 end
