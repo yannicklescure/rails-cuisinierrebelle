076989e0bd117c791afda99c65be43cc5c3f4974
Yannick Lescure <yannick.lescure@gmail.com>
2019-12-21 16:59:17 -0500

    edit host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,5 @@
 Rails.application.configure do
-  config.action_mailer.default_url_options = { host: "https://yannicklescure.com" }
+  config.action_mailer.default_url_options = { host: "http://yannicklescure.herokuapp.com" }
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
@@ -34,7 +34,6 @@ Rails.application.configure do
   # config.action_mailer.postmark_settings = { :api_token => Rails.application.secrets.postmark_api_token }
   config.action_mailer.delivery_method     = :postmark
   config.action_mailer.postmark_settings   = { api_token: ENV['POSTMARK_API_TOKEN'] }
-  # config.action_mailer.default_url_options = { host: "yannicklescure.com" }
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
