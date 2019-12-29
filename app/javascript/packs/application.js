3768b9dcfc8c89b229d9ac361967bb52918faa36
Yannick Lescure <yannick.lescure@gmail.com>
2019-12-29 17:33:47 -0500

    js regex improvement

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -12,16 +12,26 @@ if(window.innerWidth <= 768) {
 const userSignedIn = document.querySelector('body').dataset.user;
 console.log(`userSignedIn? ${userSignedIn}`);
 
-let currentPage = window.location.pathname.split("/")[1] || null;
+
+let data = window.location.href.match(/https?:\/(?<domain>\/\w+.+:\d+|\/\w+.\w+.\w+)(?<lang>\/en|\/es|\/fr)?(?<controller>\/\w+)?(?<page>\/.+)?/);
+console.log(data.groups.domain);
+
+let currentLang = data.groups.lang || null;
+if(currentLang != null) currentLang = currentLang.replace('/','');
+let currentController = data.groups.controller || null;
+if(currentController != null) currentController = currentController.replace('/','');
+let currentPage = data.groups.page || null;
+if(currentPage != null) currentPage = currentPage.replace('/','');
+
+console.log('currentLang ', currentLang);
+console.log('currentController ', currentController);
 console.log('currentPage ', currentPage);
-let currentAction = window.location.pathname.split("/")[2] || null;
-console.log('currentAction ', currentAction);
 
-if(currentPage != null && currentPage.match(/fr|es/)) navbarBrand.href += currentPage;
+if(currentLang != null) navbarBrand.href += currentLang;
 
-if(currentPage === "users") document.querySelector('.btn').classList.add('btn-secondary');
+if(currentController === "users") document.querySelector('.btn').classList.add('btn-secondary');
 
-if(userSignedIn === "false" && (currentPage === null || currentPage.match(/en|fr|es/))) {
+if(userSignedIn === "false" && (currentController === null || currentController.match(/en|fr|es/))) {
   scrollToAnchor("#recipes-cards");
   const bannerCtaBox = document.querySelector('#banner-cta-box');
   const bannerCtaBoxBtn = document.querySelector('#banner-cta-box-btn');
