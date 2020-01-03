84c2201a29b1ae8536fb88c3354bbe99aff82f03
Yannick Lescure <yannick.lescure@gmail.com>
2020-01-03 15:10:35 -0500

    add locale cookie

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,12 @@
 class ApplicationController < ActionController::Base
   before_action :set_locale, :authenticate_user!
 
-    def set_locale
-      I18n.locale = params.fetch(:locale, I18n.default_locale).to_sym
-    end
+  def set_locale
+    I18n.locale = params.fetch(:locale, I18n.default_locale).to_sym
+    session[:locale] = I18n.locale
+  end
 
-    def default_url_options
-      { locale: I18n.locale == I18n.default_locale ? nil : I18n.locale }
-    end
+  def default_url_options
+    { locale: I18n.locale == I18n.default_locale ? nil : I18n.locale }
+  end
 end
