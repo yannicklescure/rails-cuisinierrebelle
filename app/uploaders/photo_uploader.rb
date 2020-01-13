8511d05138f9c674a82d6625db057a4c3c1e0d2c
Yannick Lescure <yannick.lescure@gmail.com>
2020-01-13 10:36:30 -0500

    add filename versioning

--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -72,4 +72,14 @@ class PhotoUploader < CarrierWave::Uploader::Base
   # def filename
   #   "something.jpg" if original_filename
   # end
+
+  def filename
+    "#{secure_token}.#{file.extension}" if original_filename.present?
+  end
+
+  protected
+  def secure_token
+    var = :"@#{mounted_as}_secure_token"
+    model.instance_variable_get(var) or model.instance_variable_set(var, SecureRandom.uuid)
+  end
 end
