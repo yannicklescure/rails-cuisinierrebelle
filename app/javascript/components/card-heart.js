3c36d6d4123a2a19c330247b5006091148948328
Yannick Lescure <yannick.lescure@gmail.com>
2020-01-18 17:59:53 -0500

    JS code clean up

---
+++ app/javascript/components/card-heart.js
@@ -0,0 +1,29 @@
+const cardHeart = () => {
+  const cards = document.querySelectorAll('.card');
+  cards.forEach((card) => {
+    console.log(card.dataset);
+    const element = document.querySelector(`.fa-heart-${card.dataset.recipe}`);
+    element.classList.remove('d-none');
+    element.style.opacity = 0;
+    const cardImgTop = document.querySelector(`.card-img-top-${card.dataset.recipe}`);
+    let clickCount = 0;
+    cardImgTop.addEventListener('click', (event) => {
+      clickCount++;
+      const timeOut = () => {
+        setTimeout(() => {
+          clickCount = 0;
+        }, 400);
+      }
+      if (clickCount === 1) {
+        timeOut();
+      } else if (clickCount === 2) {
+        const liked = document.querySelector(`[data-like-recipe="${card.dataset.recipe}"]`);
+        liked.click();
+        clearTimeout(timeOut);
+        clickCount = 0;
+      }
+    });
+  });
+}
+
+export { cardHeart };
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,11 +1,11 @@
 import "bootstrap";
 import { scrollToAnchor } from "../components/scroll-to-anchor";
 import { previewImageOnFileSelect } from "../components/photo-preview";
+import { cardHeart } from "../components/card-heart";
 
 $('[data-toggle="tooltip"]').tooltip();
 
 previewImageOnFileSelect();
-// console.log(window.innerWidth);
 
 const userSignedIn = document.querySelector('body').dataset.user;
 console.log(`userSignedIn? ${userSignedIn}`);
@@ -32,52 +32,26 @@ if(window.innerWidth <= 768) {
   navbarBrand.style.padding = "5px 0";
 }
 
-if(currentController === "users") {
-  const buttons = document.querySelectorAll('.btn');
-  buttons.forEach(button => {
-    // button.classList.add('btn-secondary');
-  });
-  const modalMenuLinks = document.querySelectorAll('.modal-menu-link');
-  modalMenuLinks.forEach(modalMenuLink => {
-    modalMenuLink.classList.remove('btn-secondary');
-  });
+// if(currentController === "users") {
+//   const buttons = document.querySelectorAll('.btn');
+//   buttons.forEach(button => {
+//     // button.classList.add('btn-secondary');
+//   });
+//   const modalMenuLinks = document.querySelectorAll('.modal-menu-link');
+//   modalMenuLinks.forEach(modalMenuLink => {
+//     modalMenuLink.classList.remove('btn-secondary');
+//   });
+// }
+
+if (currentController === null || currentController.match(/users|bookmarks|index/)) {
+  cardHeart();
 }
 
-// if (currentController === null || currentController === 'users' || currentController.match(/en|fr|es/)) {
-if (currentController === null || currentController === 'users') {
-
-  const cards = document.querySelectorAll('.card');
-  cards.forEach((card) => {
-    console.log(card.dataset);
-    const element = document.querySelector(`.fa-heart-${card.dataset.recipe}`);
-    element.classList.remove('d-none');
-    element.style.opacity = 0;
-    const cardImgTop = document.querySelector(`.card-img-top-${card.dataset.recipe}`);
-    let clickCount = 0;
-    cardImgTop.addEventListener('click', (event) => {
-      clickCount++;
-      const timeOut = () => {
-        setTimeout(() => {
-          clickCount = 0;
-        }, 400);
-      }
-      if (clickCount === 1) {
-        timeOut();
-      } else if (clickCount === 2) {
-        const liked = document.querySelector(`[data-like-recipe="${card.dataset.recipe}"]`);
-        liked.click();
-        clearTimeout(timeOut);
-        clickCount = 0;
-      }
-    });
-  });
-
-  if(userSignedIn === "false") {
-    scrollToAnchor("#recipes-cards");
-    const bannerCtaBox = document.querySelector('#banner-cta-box');
-    const bannerCtaBoxBtn = document.querySelector('#banner-cta-box-btn');
-    bannerCtaBoxBtn.style.width = `${bannerCtaBox.offsetWidth}px`;
-  }
+if (currentController === null && userSignedIn === "false") {
+  scrollToAnchor("#recipes-cards");
+  const bannerCtaBox = document.querySelector('#banner-cta-box');
+  const bannerCtaBoxBtn = document.querySelector('#banner-cta-box-btn');
+  bannerCtaBoxBtn.style.width = `${bannerCtaBox.offsetWidth}px`;
 }
 
 if(currentController === 'recipes' && currentPage != null) {
@@ -103,7 +77,6 @@ if(currentController === 'recipes' && currentPage != null) {
   const replyForms = document.querySelectorAll('.no-reply');
   replyForms.forEach((replyForm) => {
     replyForm.addEventListener('click', event => {
-      // console.log(event.currentTarget.dataset.comment);
       const currentForm = document.querySelector(`#reply-form-${event.currentTarget.dataset.comment}`);
       currentForm.classList.toggle('d-none');
     });
@@ -113,7 +86,6 @@ if(currentController === 'recipes' && currentPage != null) {
   const replies = document.querySelectorAll('.reply');
   replies.forEach((reply) => {
     reply.addEventListener('click', event => {
-      // console.log(event.currentTarget.dataset.comment);
       const repliesList = document.querySelector(`#replies-list-${event.currentTarget.dataset.comment}`);
       repliesList.classList.toggle('d-none');
       viewReplies === false ? viewReplies = true : viewReplies = false;
@@ -128,7 +100,6 @@ if(currentController === 'recipes' && currentPage != null) {
   console.log(repliesReply);
   repliesReply.forEach((reply) => {
     reply.addEventListener('click', event => {
-      // console.log(event.currentTarget.dataset.comment);
       const replyForm = document.querySelector(`#reply-reply-form-${event.currentTarget.dataset.comment}`);
       replyForm.classList.toggle('d-none');
     });
