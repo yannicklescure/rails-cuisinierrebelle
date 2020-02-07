27eae7695018e61455843ecb4d4e21859101be40
Yannick Lescure <yannick.lescure@gmail.com>
2020-02-07 11:42:47 -0500

    improve code segmentation + add smarphone share button

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -106,3 +106,27 @@ if(device.match(/smartphone|phablet/)) {
     navbarSearch.classList.toggle('d-none');
   });
 }
+
+const shareBtn = document.querySelector('#share-btn');
+if(shareBtn) {
+  let url = document.location.href;
+  const canonicalElement = document.querySelector('link[rel=canonical]');
+  if (canonicalElement !== null) {
+      url = canonicalElement.href;
+  }
+  const shareData = {
+    // title: 'MDN',
+    // text: 'Learn web development on MDN!',
+    url: url,
+  }
+
+  // Must be triggered some kind of "user activation"
+  shareBtn.addEventListener('click', async () => {
+    try {
+      await navigator.share(shareData)
+    } catch(err) {
+      console.log('Error: ' + e);
+    }
+    console.log('MDN shared successfully');
+  });
+}
