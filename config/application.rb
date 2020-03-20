1abea25f282f89c20c892ae1b99fbbd9ac4cc928
Yannick Lescure <yannick.lescure@gmail.com>
2020-03-20 10:28:05 -0400

    update

--- config/application.rb
+++ config/application.rb
@@ -25,7 +25,7 @@ module RailsCuisinierRebelle
           generate.test_framework  :test_unit, fixture: false
         end
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults "6.0.2.2"
+    config.load_defaults "6.0"
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
