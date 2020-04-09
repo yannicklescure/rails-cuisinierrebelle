5a1ecf8303c75a5131d79c48a2f4e4b6b1b9bfd3
Yannick Lescure <yannick.lescure@gmail.com>
2020-04-09 13:07:49 -0400

    change icons

--- app/javascript/components/admin-nav.js
+++ app/javascript/components/admin-nav.js
@@ -4,15 +4,11 @@ const menuOpener = (menuOpen) => {
   adminNavItems.forEach(adminNavItem => {
     if(!menuOpen) {
       adminNavItem.classList.add('d-none');
-      adminNavBtnIcon.innerHTML = `<svg class="bi bi-chevron-right" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-          <path fill-rule="evenodd" d="M4.646 1.646a.5.5 0 01.708 0l6 6a.5.5 0 010 .708l-6 6a.5.5 0 01-.708-.708L10.293 8 4.646 2.354a.5.5 0 010-.708z" clip-rule="evenodd"/>
-        </svg>`;
+      adminNavBtnIcon.innerHTML = `<i class="material-icons md-18 mb-1">keyboard_arrow_right</i>`;
     }
     else {
       adminNavItem.classList.remove('d-none');
-      adminNavBtnIcon.innerHTML = `<svg class="bi bi-chevron-left" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-          <path fill-rule="evenodd" d="M11.354 1.646a.5.5 0 010 .708L5.707 8l5.647 5.646a.5.5 0 01-.708.708l-6-6a.5.5 0 010-.708l6-6a.5.5 0 01.708 0z" clip-rule="evenodd"/>
-        </svg>`;
+      adminNavBtnIcon.innerHTML = `<i class="material-icons md-18 mb-1">keyboard_arrow_left</i>`;
     }
   });
 }
