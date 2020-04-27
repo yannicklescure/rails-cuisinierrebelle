7b424732e188614d98282428260704557bce06ea
Yannick Lescure <yannick.lescure@gmail.com>
2020-04-26 21:38:46 -0400

    improve UI/UX

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -183,15 +183,18 @@ if(device.match(/smartphone|phablet/)) {
 const top100Title = document.querySelector('#top-100-title');
 const top100TitleBar = document.querySelector('#top-100-title-bar');
 if (top100TitleBar) {
+  const top100TitleBarOffsetTop = top100TitleBar.offsetTop;
+  const navbarHeight = parseInt(document.querySelector('#navbar-main').offsetHeight);
+  const top100TitleMarginBottom = parseInt(window.getComputedStyle(top100Title).getPropertyValue('margin-bottom').replace('px',''));
   window.addEventListener('scroll', () => {
-    const navbarHeight = parseInt(document.querySelector('#navbar-main').offsetHeight);
-    const top100TitleMarginBottom = parseInt(window.getComputedStyle(top100Title).getPropertyValue('margin-bottom').replace('px',''));
-    if (window.scrollY > navbarHeight + top100Title.offsetHeight + top100TitleMarginBottom) {
+    if (window.scrollY > top100TitleBarOffsetTop - navbarHeight) {
       top100TitleBar.style.top = `${navbarHeight}px`;
       top100TitleBar.classList.add('fixed-top');
-    } else {
+      top100Title.style.paddingBottom = `${top100TitleBar.offsetHeight}px`;
+    } else if (window.scrollY < top100TitleBarOffsetTop + navbarHeight) {
       top100TitleBar.style.top = null;
       top100TitleBar.classList.remove('fixed-top');
+      top100Title.style.paddingBottom = null;
     }
   });
 }
