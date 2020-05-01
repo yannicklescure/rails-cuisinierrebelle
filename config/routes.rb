cf83f8f1910dbe411a28e100bbaa3d0bcd57ac95
Yannick Lescure <yannick.lescure@gmail.com>
2020-05-01 11:21:24 -0400

    fix

--- config/routes.rb
+++ config/routes.rb
@@ -85,6 +85,8 @@ Rails.application.routes.draw do
   scope '(:locale)', locale: /en|fr|es/ do
     root to: 'pages#home'
 
+    # https://www.cuisinierrebelle.com/r/faire-son-pain-a-la-maison-dbda3253-e38e-491b-b338-313805d27311
+    get '/r/faire-son-pain-a-la-maison-dbda3253-e38e-491b-b338-313805d27311', to: redirect('/r/pain-maison-sans-petrissage-et-sans-faconnage')
     get '/:locale/r/faire-son-pain-a-la-maison-dbda3253-e38e-491b-b338-313805d27311', to: redirect('/%{locale}/r/pain-maison-sans-petrissage-et-sans-faconnage')
 
     get '/conversion', to: 'pages#conversion', as: 'conversion'
