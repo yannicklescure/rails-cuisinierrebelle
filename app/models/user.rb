99334c6cda457cd965f1b1c137ab5b410942980a
Yannick Lescure <yannick.lescure@gmail.com>
2020-05-24 11:01:19 -0400

    enable user search trigram

--- app/models/user.rb
+++ app/models/user.rb
@@ -58,10 +58,11 @@ class User < ApplicationRecord
   friendly_id :name, use: :slugged
 
   include PgSearch::Model
-  # PgSearch.multisearch_options = {
-  #   using: [:tsearch, :trigram],
-  #   # ignoring: :accents
-  # }
+  PgSearch.multisearch_options = {
+    # using: [:tsearch, :trigram],
+    using: [:trigram],
+    # ignoring: :accents
+  }
   multisearchable against: [:name, :first_name, :last_name]
   private
 
