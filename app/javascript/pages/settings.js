155513ef4772d158b02dd553d58e1cf2203fc0cc
Yannick Lescure <yannick.lescure@gmail.com>
2020-06-16 09:26:18 -0400

    minor UI/UX fix

--- app/javascript/pages/settings.js
+++ app/javascript/pages/settings.js
@@ -1,20 +1,22 @@
 import { mailchimp } from "../services/mailchimp";
 import { notification } from "../services/notification";
+import { cookiesToObject } from "../components/cookies";
 
 export const settings = () => {
-    const userId = parseInt(document.querySelector('body').dataset.userId);
-    const initMailchimp = {
-      user_id: userId,
-      user_email: cookies.user_email,
-      user_token: cookies.user_token,
-      url: '/api/v1/mailchimp'
-    };
-    mailchimp(initMailchimp);
-    const initNotification = {
-      user_id: userId,
-      user_email: cookies.user_email,
-      user_token: cookies.user_token,
-      url: '/api/v1/notification'
-    };
-    notification(initNotification);
+  const cookies = cookiesToObject(document.cookie);
+  const userId = parseInt(document.querySelector('body').dataset.userId);
+  const initMailchimp = {
+    user_id: userId,
+    user_email: cookies.user_email,
+    user_token: cookies.user_token,
+    url: '/api/v1/mailchimp'
+  };
+  mailchimp(initMailchimp);
+  const initNotification = {
+    user_id: userId,
+    user_email: cookies.user_email,
+    user_token: cookies.user_token,
+    url: '/api/v1/notification'
+  };
+  notification(initNotification);
 }
