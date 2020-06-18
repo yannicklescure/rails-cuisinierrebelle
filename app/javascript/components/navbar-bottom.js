267031ffcb20745bd3bcbd2aefee1e0a1e3a66fe
Yannick Lescure <yannick.lescure@gmail.com>
2020-06-17 21:00:12 -0400

    improve UI/UX

--- app/javascript/components/navbar-bottom.js
+++ app/javascript/components/navbar-bottom.js
@@ -45,16 +45,15 @@ export const navbarBottom = () => {
 
     // const root = document.querySelector('#root');
 
-
     fetchUserData(init, options).then(data => {
-      console.log(data)
+      // console.log(data)
       let navbar = `<div class="fixed-bottom d-flex border-top justify-content-center align-items-center bg-white py-2 px-4">`;
-      navbar += `<a href="/" class="text-dark"><span class="material-icons md-32 d-flex px-3">home</span></a>`;
-      navbar += `<a href="/top100" class="text-dark"><span class="material-icons md-32 d-flex px-3">whatshot</span></a>`;
+      navbar += `<a href="/" class="text-dark text-decoration-none"><span class="material-icons md-32 d-flex px-3">home</span></a>`;
+      navbar += `<a href="/top100" class="text-dark text-decoration-none"><span class="material-icons md-32 d-flex px-3">whatshot</span></a>`;
       // navbar += `<a href="" class="text-dark"><span class="material-icons md-32 d-flex px-3">notifications</span></a>`;
-      navbar += `<a href="/r/new" class="text-dark"><span class="material-icons md-32 d-flex px-3">add_box</span></a>`;
-      navbar += `<a href="/u/${data.user.auth.slug}/bookmarks" class="text-dark"><span class="material-icons md-32 d-flex px-3">bookmarks</span></a>`;
-      navbar += `<a href="/u/${data.user.auth.slug}" class="text-dark px-3"><img src="${data.user.auth.image.thumb.url}" width="32" height="32" class="border border-dark rounded-circle"></a>`;
+      navbar += `<a href="/r/new" class="text-dark text-decoration-none"><span class="material-icons md-32 d-flex px-3">add_box</span></a>`;
+      navbar += `<a href="/u/${data.user.auth.slug}/bookmarks" class="text-dark text-decoration-none"><span class="material-icons md-32 d-flex px-3">bookmarks</span></a>`;
+      navbar += `<a href="/u/${data.user.auth.slug}" class="text-dark px-3 text-decoration-none"><img src="${data.user.auth.image.thumb.url}" width="32" height="32" class="border border-dark rounded-circle"></a>`;
       navbar += `</div>`;
       body.insertAdjacentHTML('afterBegin', navbar);
     });
