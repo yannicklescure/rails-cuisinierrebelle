3e44410708b7bfd0627550e5e0a56863f15818e3
Yannick Lescure <yannick.lescure@gmail.com>
2020-07-03 13:05:47 -0400

    fix js load

--- app/javascript/pages/top100.js
+++ app/javascript/pages/top100.js
@@ -1,20 +1,23 @@
 export const top100 = () => {
-  const top100Title = document.querySelector('#top-100-title');
-  const top100TitleBar = document.querySelector('#top-100-title-bar');
-  if (top100TitleBar) {
-    const top100TitleBarOffsetTop = top100TitleBar.offsetTop;
-    const navbarHeight = parseInt(document.querySelector('#navbar-main').clientHeight);
-    // const top100TitleMarginBottom = parseInt(window.getComputedStyle(top100Title).getPropertyValue('margin-bottom').replace('px',''));
-    window.addEventListener('scroll', () => {
-      if (window.scrollY > top100TitleBarOffsetTop) {
-        top100TitleBar.style.top = `${navbarHeight}px`;
-        top100TitleBar.classList.add('fixed-top');
-        top100Title.style.paddingBottom = `${top100TitleBar.clientHeight}px`;
-      } else if (window.scrollY < top100TitleBarOffsetTop) {
-        top100TitleBar.style.top = null;
-        top100TitleBar.classList.remove('fixed-top');
-        top100Title.style.paddingBottom = null;
-      }
-    });
-  }
+  document.addEventListener('scroll', (event) => {
+    const top100Title = document.querySelector('#top-100-title');
+    const top100TitleBar = document.querySelector('#top-100-title-bar');
+    if (top100TitleBar) {
+      const top100TitleBarOffsetTop = top100TitleBar.offsetTop;
+      const navbarHeight = parseInt(document.querySelector('#navbar-main').clientHeight);
+      // console.log(navbarHeight);
+      // const top100TitleMarginBottom = parseInt(window.getComputedStyle(top100Title).getPropertyValue('margin-bottom').replace('px',''));
+      window.addEventListener('scroll', () => {
+        if (window.scrollY > top100TitleBarOffsetTop) {
+          top100TitleBar.style.top = `${navbarHeight}px`;
+          top100TitleBar.classList.add('fixed-top');
+          top100Title.style.paddingBottom = `${top100TitleBar.clientHeight}px`;
+        } else if (window.scrollY < top100TitleBarOffsetTop) {
+          top100TitleBar.style.top = null;
+          top100TitleBar.classList.remove('fixed-top');
+          top100Title.style.paddingBottom = null;
+        }
+      });
+    }
+  });
 }
