86249fa95c3b25dfae48a04455a25b63b4ffda8e
Yannick Lescure <yannick.lescure@gmail.com>
2020-07-08 17:10:20 -0400

    sort likes list

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -4,6 +4,6 @@ class NotificationsController < ApplicationController
     @user = policy_scope(User).find(current_user.id)
     # binding.pry
     @recipes = Recipe.where(user: current_user)
-    @likes = Like.where(recipe: @recipes).last(100)
+    @likes = Like.where(recipe: @recipes).last(100).sort_by { |u| u.created_at }.reverse
   end
 end
