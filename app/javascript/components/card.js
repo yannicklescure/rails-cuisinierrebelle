beb4991bb98090d10f74b41de19c94c2e2d09c27
yannick lescure <yannick.lescure@gmail.com>
2020-07-31 10:28:18 -0400

    improve UI/UX

--- app/javascript/components/card.js
+++ app/javascript/components/card.js
@@ -66,7 +66,6 @@ export const card = (params, callback = () => {}) => {
       // console.log(`count ${commentsCount}`);
 
       const card = `
-        <div class="col-md-4 col-lg-3 col-xl-2 p-2">
           <div class="card border-0" data-recipe="${recipe.id}">
             <div class="card-header py-1 px-0 border-0 bg-white rounded">
               <div class="d-flex justify-content-start align-items-center">
@@ -102,7 +101,6 @@ export const card = (params, callback = () => {}) => {
               <div class="card-text font-weight-lighter" style="font-size: 90%">${recipe.description}</div>
             </div>
           </div>
-        </div>
       `;
       const root = document.querySelector('#root');
       root.insertAdjacentHTML('beforeEnd', card);
