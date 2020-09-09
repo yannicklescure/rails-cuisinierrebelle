f5e424799b40cf1ee404aff4dbb6c482b6fa3a91
Yannick Lescure <yannick.lescure@gmail.com>
2020-09-09 00:18:38 -0400

    reduce fontawesome css weight

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -25,6 +25,29 @@ environment.plugins.append(
   })
 )
 
+const fs = require('fs')
+
+let iconFontVersion = 'dev'
+try {
+  iconFontVersion = fs.readFileSync('REVISION', 'utf-8').trim()
+}
+catch(_error) {
+  // okay, probably in dev mode
+}
+
+environment.loaders.prepend('fa-font', {
+  test: /fa-.*\.(woff2|woff|eot|svg|ttf)$/,
+  use: [{
+    loader: 'file-loader',
+    options: {
+      name: '[path][name]-[hash]-' + iconFontVersion + '.[ext]',
+      context: join(config.source_path),
+    }
+  }]
+})
+
+environment.loaders.get('file').exclude = /fa-.*\.(woff2|woff|eot|svg|ttf)$/
+
 environment.splitChunks((config) => Object.assign({}, config, { optimization: { splitChunks: { chunks: 'all' } }}))
 // environment.splitChunks()
 
