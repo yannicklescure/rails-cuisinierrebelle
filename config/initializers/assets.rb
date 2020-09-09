7514ce852b680453c6ba4f6c6c8142c5a6f9a949
yannick lescure <yannick.lescure@gmail.com>
2020-09-09 10:25:53 -0400

    improve code performance

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -15,3 +15,10 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 
 Rails.application.config.assets.precompile += %w( application-mailer.scss )
 Rails.configuration.assets.precompile += %w[serviceworker.js manifest.json]
+
+# Adding Webfonts to the Asset Pipeline
+Rails.application.config.assets.precompile << Proc.new { |path|
+  if path =~ /\.(eot|svg|ttf|woff)\z/
+    true
+  end
+}
