4c4a4d8bae8201ee8f654ce7cf5ce1ed59eea322
Yannick Lescure <yannick.lescure@gmail.com>
2020-09-14 16:14:45 -0400

    remove assets folder

--- public/assets/google_analytics-de0bc6fccc664e6f08ef0c265fc95c2cfd384af3cf47e727baca064549924f97.js
+++
@@ -1,1 +0,0 @@
-document.addEventListener("turbolinks:load",function(t){"function"==typeof gtag&&gtag("config","",{page_location:t.data.url})});
--- public/assets/serviceworker-companion-3ae2d73c001eb3e2acdcf19868e8991eac4a913f63192f8a46996e11c3ed732c.js
+++
@@ -1,1 +0,0 @@
-navigator.serviceWorker&&navigator.serviceWorker.register("/serviceworker.js",{scope:"./"}).then(function(){console.log("[Companion]","Service worker registered!")});
