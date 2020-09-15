06ac2c964c187745689dcf364c20d39527a17918
yannick lescure <yannick.lescure@gmail.com>
2020-09-15 08:36:04 -0400

    update assets

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -20,35 +20,35 @@ environment.plugins.prepend('Provide',
 
 environment.config.merge()
 
-// const FontminPlugin = require('fontmin-webpack')
+const FontminPlugin = require('fontmin-webpack')
 
-// environment.plugins.append(
-//   'Fontmin', new FontminPlugin({
-//     autodetect: true,
-//   })
-// )
+environment.plugins.append(
+  'Fontmin', new FontminPlugin({
+    autodetect: true,
+  })
+)
 
-// const bootstrap = {
-//   test: /\.(scss)$/,
-//   use: [{
-//     loader: 'style-loader', // inject CSS to page
-//   }, {
-//     loader: 'css-loader', // translates CSS into CommonJS modules
-//   }, {
-//     loader: 'postcss-loader', // Run post css actions
-//     options: {
-//       plugins: function () { // post css plugins, can be exported to postcss.config.js
-//         return [
-//           require('precss'),
-//           require('autoprefixer')
-//         ];
-//       }
-//     }
-//   }, {
-//     loader: 'sass-loader' // compiles Sass to CSS
-//   }]
-// }
-// environment.loaders.append('bootstrap', bootstrap)
+const bootstrap = {
+  test: /\.(scss)$/,
+  use: [{
+    loader: 'style-loader', // inject CSS to page
+  }, {
+    loader: 'css-loader', // translates CSS into CommonJS modules
+  }, {
+    loader: 'postcss-loader', // Run post css actions
+    options: {
+      plugins: function () { // post css plugins, can be exported to postcss.config.js
+        return [
+          require('precss'),
+          require('autoprefixer')
+        ];
+      }
+    }
+  }, {
+    loader: 'sass-loader' // compiles Sass to CSS
+  }]
+}
+environment.loaders.append('bootstrap', bootstrap)
 
 environment.splitChunks((config) => Object.assign({}, config, {
   optimization: {
