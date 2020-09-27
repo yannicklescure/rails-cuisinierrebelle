6bb14e434c797177fb5398ee6bcc45ca4c8f08ae
yannick lescure <yannick.lescure@gmail.com>
2020-09-27 15:31:43 -0400

    improve UI/UX

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -47,44 +47,7 @@ environment.config.merge()
 const UglifyJsPlugin = require("uglifyjs-webpack-plugin")
 // environment.plugins.delete("UglifyJs")
 // environment.plugins.append("UglifyJs", new UglifyJsPlugin())
-environment.config.set('optimization.minimizer', [new UglifyJsPlugin(
-    uglifyOptions: {
-      compress: {
-        arrows: false,
-        booleans: false,
-        cascade: false,
-        collapse_vars: false,
-        comparisons: false,
-        computed_props: false,
-        hoist_funs: false,
-        hoist_props: false,
-        hoist_vars: false,
-        if_return: false,
-        inline: false,
-        join_vars: false,
-        keep_infinity: true,
-        loops: false,
-        negate_iife: false,
-        properties: false,
-        reduce_funcs: false,
-        reduce_vars: false,
-        sequences: false,
-        side_effects: false,
-        switches: false,
-        top_retain: false,
-        toplevel: false,
-        typeofs: false,
-        unused: false,
-
-        // Switch off all types of compression except those needed to convince
-        // react-devtools that we're using a production build
-        conditionals: true,
-        dead_code: true,
-        evaluate: true,
-      },
-      mangle: true,
-    },
-)])
+environment.config.set('optimization.minimizer', [new UglifyJsPlugin()]);
 
 const CompressionPlugin = require('compression-webpack-plugin');
 
