16dd8091c9789c4c31edf76b58df6b136722e89a
Yannick Lescure <yannick.lescure@gmail.com>
2020-09-28 22:06:57 -0400

    improve UI/UX

---
+++ app/javascript/serviceworker-companion.js
@@ -0,0 +1,11 @@
+if (navigator.serviceWorker) {
+  console.log('[serviceWorker] Hello');
+  navigator.serviceWorker.register('/serviceworker.js', { scope: './' })
+    .then(function(reg) {
+      console.log('[Companion]', 'Service worker registered!');
+    })
+    .catch((error) => {
+        console.log('Service Worker failed to register');
+        console.log(error);
+    });
+}
