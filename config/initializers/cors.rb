fe2a19eed461cfeec372548afd7dcc7cdcecd496
Yannick Lescure <yannick.lescure@gmail.com>
2020-11-13 00:10:48 -0500

    improve code + fix bugs

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -14,12 +14,21 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
           https://www.cuisinierrebelle.com
         ]
 
-    resource '/assets/*', headers: :any, methods: [:get, :post, :options]
+    # All asset requests should be to rails prefixed assets paths
+    # serverd from the asset pipeline (e.g.: "/assets/*" by default)
+    resource "#{Rails.application.config.assets.prefix}/*",
+      # Allow any request headers to be sent in the asset request
+      # https://developer.mozilla.org/en-US/docs/Web/HTTP/Access_control_CORS#Access-Control-Allow-Headers
+      headers: :any,
+      # All asset fetches should be via GET
+      # Support OPTIONS for pre-flight requests
+      # https://developer.mozilla.org/en-US/docs/Web/HTTP/Access_control_CORS#Preflighted_requests
+      methods: [:get, :options]
 
     resource  '/api/v1/*',
-              headers: %w(Authorization),
-              methods: :any,
-              expose: %w(Authorization)
+      headers: %w(Authorization),
+      methods: :any,
+      expose: %w(Authorization)
 
     # resource  '*',
     #           headers: :any,
