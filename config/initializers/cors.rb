c161f64b11a5798ba436ec0b63e2bec58a027485
Yannick Lescure <yannick.lescure@gmail.com>
2020-11-12 23:58:20 -0500

    improve code + fix bugs

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,17 +7,24 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
   # end
 
   allow do
-    origins   '*'
+    # origins   '*'
+
+    origins %w[
+          http://www.cuisinierrebelle.com
+          https://www.cuisinierrebelle.com
+        ]
+
+    resource '/assets/*', headers: :any, methods: [:get, :post, :options]
 
     resource  '/api/v1/*',
               headers: %w(Authorization),
               methods: :any,
               expose: %w(Authorization)
 
-    resource  '*',
-              headers: :any,
-              methods: [:get, :post, :delete, :put, :patch, :options, :head],
-              max_age: 0
+    # resource  '*',
+    #           headers: :any,
+    #           methods: [:get, :post, :delete, :put, :patch, :options, :head],
+    #           max_age: 0
   end
 end
 
