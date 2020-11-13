20f06ca995784f47068983a283431e97bc2ad593
Yannick Lescure <yannick.lescure@gmail.com>
2020-11-13 00:19:53 -0500

    improve code + fix bugs

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -22,4 +22,4 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
   end
 end
 
-Rails.application.config.hosts << "www.cuisinierrebelle.com"
+Rails.application.config.hosts << "cuisinierrebelle.com"
