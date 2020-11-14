5c593b9d23c572fd4864b4afa22cc4cd28ca1f97
Yannick Lescure <yannick.lescure@gmail.com>
2020-11-14 09:56:43 -0500

    improve code + fix bugs

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,1 +1,10 @@
 //= require serviceworker-companion
+
+if ((/localhost/).test(document.domain)) document.domain = "localhost";
+else console.log = function() {}
+
+if ((/.*cuisinierrebelle\.com/).test(document.domain)) {
+  document.domain = "cuisinierrebelle.com";
+}
+console.log(document.domain);
+console.log(window.location.hostname);
