fdeaf774f4c50f941c18fcebe884ad7bab4eb315
Yannick Lescure <yannick.lescure@gmail.com>
2020-11-21 15:09:33 -0500

    improve code + fix bugs

--- app/javascript/packs/hello_vue.js
+++ app/javascript/packs/hello_vue.js
@@ -71,7 +71,7 @@ Vue.use(VuejsDialog);
 import VueToast from 'vue-toast-notification'
 // Import one of the available themes
 //import 'vue-toast-notification/dist/theme-default.css'
-import 'vue-toast-notification/dist/theme-sugar.css'
+// import 'vue-toast-notification/dist/theme-sugar.css'
 
 Vue.use(VueToast)
 // //Vue.$toast.open({/* options */});
