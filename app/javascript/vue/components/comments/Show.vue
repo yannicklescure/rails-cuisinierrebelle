137e9bd6ec58024f55692bf25601cea3b82205bd
Yannick Lescure <yannick.lescure@gmail.com>
2020-12-06 10:10:14 -0500

    improve code + fix bugs

--- app/javascript/vue/components/comments/Show.vue
+++ app/javascript/vue/components/comments/Show.vue
@@ -27,38 +27,12 @@
     <div v-else class="mt-2 bg-light rounded p-3">
       <vue-markdown :source="item.content" class="text-break" />
     </div>
-    <div v-if="isAuthenticated" class="mt-2 d-flex align-items-center">
-      <comment-like :item="item" :type="type" />
-      <div
-        v-if="item.user.id === currentUser.id"
-        v-on:click="commentEdit"
-        class="d-flex text-muted mx-2 mouse-pointer"
-        data-toggle="tooltip"
-        data-placement="bottom"
-        :title="$t('comment.edit')"
-      >
-        <span class="material-icons md-16">edit</span>
-      </div>
-      <div
-        v-if="item.user.id === currentUser.id"
-        v-on:click="isComment2Destroy"
-        class="d-flex text-muted mx-2 mouse-pointer"
-        data-toggle="tooltip"
-        data-placement="bottom"
-        :title="$t('comment.destroy')"
-      >
-        <span class="material-icons md-16">delete</span>
-      </div>
-      <div
-        v-on:click="commentReply"
-        class="d-flex text-muted mx-2 mouse-pointer"
-        data-toggle="tooltip"
-        data-placement="bottom"
-        :title="$t('comment.reply')"
-      >
-        <span class="material-icons md-16">reply</span>
-      </div>
-    </div>
+    <comment-buttons
+      :item="item"
+      :type="type"
+      v-on:commentEdit="commentEdit"
+      v-on:commentReply="commentReply"
+    />
     <div v-if="reply">
       <comment-form
         :item="item"
@@ -77,8 +51,9 @@ import { mapGetters } from 'vuex'
 import { isMobile } from 'mobile-device-detect'
 // import CommentForm from './Form.vue'
 // import CommentLike from '../buttons/CommentLike.vue'
+const CommentButtons = () => import('./Buttons.vue')
 const CommentForm = () => import('./Form.vue')
-const CommentLike = () => import('../buttons/CommentLike.vue')
+// const CommentLike = () => import('../buttons/CommentLike.vue')
 
 export default {
   name: 'Comment',
@@ -90,8 +65,9 @@ export default {
     }
   },
   components: {
+    CommentButtons,
     CommentForm,
-    CommentLike,
+    // CommentLike,
     VueMarkdown,
   },
   computed: {
@@ -101,16 +77,16 @@ export default {
     },
   },
   methods: {
+    commentEdit (value) {
+      this.edit = value
+    },
+    commentReply (value) {
+      // console.log('reply')
+      this.reply = value
+    },
     editActionAttr () {
       if (this.item.commentId) return 'REPLY_EDIT'
       else return 'COMMENT_EDIT'
-    },
-    commentReply () {
-      console.log('reply')
-      this.reply = true
-    },
-    commentEdit () {
-      this.edit = true
     },
     commentDrop () {
       this.edit = false
@@ -137,66 +113,6 @@ export default {
       else {
         this.item.content = payload.data.content
       }
-    },
-    isComment2Destroy () {
-      let message = this.$t('comment.are_you_sure')
-
-      let options = {
-          // html: false, // set to true if your message contains HTML tags. eg: "Delete <b>Foo</b> ?"
-          // loader: false, // set to true if you want the dailog to show a loader after click on "proceed"
-          // reverse: false, // switch the button positions (left to right, and vise versa)
-          okText:  this.$t('comment.destroy'),
-          cancelText: this.$t('comment.cancel'),
-          // animation: 'zoom', // Available: "zoom", "bounce", "fade"
-          // type: 'basic', // coming soon: 'soft', 'hard'
-          // verification: 'continue', // for hard confirm, user will be prompted to type this to enable the proceed button
-          // verificationHelp: 'Type "[+:verification]" below to confirm', // Verification help text. [+:verification] will be matched with 'options.verification' (i.e 'Type "continue" below to confirm')
-          // clicksCount: 3, // for soft confirm, user will be asked to click on "proceed" btn 3 times before actually proceeding
-          backdropClose: true, // set to true to close the dialog when clicking outside of the dialog window, i.e. click landing on the mask
-          customClass: '' // Custom class to be injected into the parent node for the current dialog instance
-      };
-
-      this.$dialog
-        .confirm(message, options)
-        .then(dialog => {
-          console.log('Clicked on proceed')
-          console.log(dialog)
-          this.commentDestroy()
-        })
-        .catch(() => {
-          console.log('Clicked on cancel')
-        });
-    },
-    commentDestroy () {
-      let actionAttr = ''
-      let payload = {}
-      if (this.type === 'comment') {
-        // console.log(`delete comment ${ this.item.id }`)
-        payload = {
-          comment_id: this.item.id,
-          recipe_id: this.item.recipe.id,
-          type: this.type,
-        }
-        actionAttr = 'COMMENT_DELETE'
-      }
-      if (this.type === 'reply') {
-        // console.log(`delete comment ${ this.item.id }`)
-        payload = {
-          comment_id: this.item.commentId,
-          recipe_id: this.item.recipeId,
-          id: this.item.id,
-          type: this.type,
-        }
-        actionAttr = 'REPLY_DELETE'
-      }
-      this.$store
-        .dispatch(actionAttr, payload)
-        .then( response => {
-          console.log(response)
-          if (response.status === 204) {
-            this.$emit('commentDestroyed', payload)
-          }
-        })
     },
     timeAgo (time) {
       const between = Math.trunc((new Date().getTime() - time) / 1000)
