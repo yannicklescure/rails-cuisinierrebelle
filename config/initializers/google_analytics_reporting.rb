8b584727bcb5b9e4ddb170be212b556a46a576e3
Yannick Lescure <yannick.lescure@gmail.com>
2020-12-11 12:35:30 -0500

    improve code + fix bugs

--- config/initializers/google_analytics_reporting.rb
+++ config/initializers/google_analytics_reporting.rb
@@ -1,11 +1,9 @@
-require 'open-uri'
-
 # Create a Google Analytics Reporting service
 service = Google::Apis::AnalyticsreportingV4::AnalyticsReportingService.new
 
 # Create service account credentials
 credentials = Google::Auth::ServiceAccountCredentials.make_creds(
-  json_key_io: open('https://assets.cuisinierrebelle.com/cuisinier-rebelle-b05e87863f39.json'),
+  json_key_io: URI.open('https://assets.cuisinierrebelle.com/cuisinier-rebelle-b05e87863f39.json'),
   scope: 'https://www.googleapis.com/auth/analytics.readonly'
 )
 
