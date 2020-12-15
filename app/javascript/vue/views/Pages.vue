a09a55d6ec830f35058a3f8a17d1b86dbba17635
Yannick Lescure <yannick.lescure@gmail.com>
2020-12-14 19:52:56 -0500

    improve code + fix bugs

--- app/javascript/vue/views/Pages.vue
+++ app/javascript/vue/views/Pages.vue
@@ -1,7 +1,24 @@
 <template>
   <div :style="{ paddingTop: navbarHeight + 'px' }" class="container">
     <h1>Pages</h1>
-    <table class="table table-sm table-striped">
+    <div class="d-flex my-3">
+      <div class="mr-3">
+        <label for="inputPageLocale">{{ $t('page.new.locale') }}</label>
+      </div>
+      <div class="custom-control custom-radio custom-control-inline">
+        <input type="radio" id="localeRadioInlineFr" name="localeRadioInline" class="custom-control-input" value="fr" v-model="locale">
+        <label class="custom-control-label" for="localeRadioInlineFr">Fr</label>
+      </div>
+      <div class="custom-control custom-radio custom-control-inline">
+        <input type="radio" id="localeRadioInlineEn" name="localeRadioInline" class="custom-control-input" value="en" v-model="locale">
+        <label class="custom-control-label" for="localeRadioInlineEn">En</label>
+      </div>
+      <div class="custom-control custom-radio custom-control-inline">
+        <input type="radio" id="localeRadioInlineEs" name="localeRadioInline" class="custom-control-input" value="es" v-model="locale">
+        <label class="custom-control-label" for="localeRadioInlineEs">Es</label>
+      </div>
+    </div>
+    <table class="table table-striped">
       <thead>
         <tr>
           <th scope="col">{{ $t('pages.locale') }}</th>
@@ -12,10 +29,10 @@
         v-for="(page, index) in pages"
         :key="page.id"
       >
-        <tr>
+        <tr v-if="page.locale === locale">
           <th scope="row">{{ page.locale }}</th>
           <td>
-            <router-link :to="'/p/' + page.slug" >{{ page.title }}</router-link>
+            <router-link :to="'/p/' + page.slug">{{ page.title }}</router-link>
           </td>
         </tr>
       </tbody>
@@ -31,6 +48,7 @@ export default {
   data () {
     return {
       componentKey: 0,
+      locale: 'fr',
     }
   },
   methods: {
