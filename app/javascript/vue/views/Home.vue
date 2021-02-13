67092790a7c99604507b217ee70b35105cc55454
yannick lescure <yannick.lescure@gmail.com>
2021-02-12 21:28:25 -0500

    improve code + fix bugs

--- app/javascript/vue/views/Home.vue
+++ app/javascript/vue/views/Home.vue
@@ -164,8 +164,8 @@ export default {
       setTimeout(() => {
         // this.loadMore()
         // while (!this.items && this.data.length === 0) this.loadMore()
+        // console.log(this.data)
       }, 1000)
-      // console.log(this.data)
     })
   }
 }
